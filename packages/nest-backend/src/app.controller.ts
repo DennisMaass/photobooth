29dccdca7605f957a8214c3976747f67b108ba5d
Dennis Maaß <dennis.maass@diva-e.com>
2022-06-08 19:13:56 +0200

    feat: add infos endpoint

--- packages/nest-backend/src/app.controller.ts
+++ packages/nest-backend/src/app.controller.ts
@@ -10,6 +10,17 @@ export class AppController {
     private configService: ConfigService,
   ) {}
 
+  @Get('/infos')
+  getInfos(){
+    return {
+      stage: this.configService.get<string>('STAGE'),
+      eventName: this.configService.get<string>('EVENT_NAME'),
+      originalPath: this.configService.get<string>('ORIGINAL_PATH'),
+      printPath: this.configService.get<string>('PRINT_PATH'),
+      previewPath: this.configService.get<string>('PREVIEW_PATH'),
+    }
+  }
+
   @Post()
   async take(): Promise<{ id: string }> {
     console.debug('[AppController][take]')
