ff9436e2fca3f9208d437cf6a368bda75134ac72
Dennis Maaß <dennis.maass@diva-e.com>
2022-06-09 23:30:19 +0200

    fix: pic res

--- packages/nest-backend/src/command.service.ts
+++ packages/nest-backend/src/command.service.ts
@@ -4,16 +4,18 @@ const { exec } = require('child_process');
 @Injectable()
 export class CommandService {
   async exec(command): Promise<void> {
+    console.error('[CommandService][exec]',command)
     return new Promise((resolve, reject) => {
       exec(command, (error, stdout, stderr) => {
         if (error) {
-          console.error("[CommandService][exec]",error)
+          console.error("[CommandService][exec] error",error)
           reject(error);
         }
         if (stderr) {
-          console.error("[CommandService][exec]",stderr)
+          console.error("[CommandService][exec] error",stderr)
           reject(stderr);
         }
+        console.error('[CommandService][exec] done',command)
         resolve();
       });
     });
