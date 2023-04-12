eeca8c80bb0ea7dd45b1f0703a43894fa6622ef9
Dennis <dennis.maass.privat@googlemail.com>
2023-04-12 21:22:30 +0200

    feat: add ws ssl

--- packages/nest-backend/src/main.ts
+++ packages/nest-backend/src/main.ts
@@ -9,19 +9,34 @@ async function bootstrap(
   port: number,
   wsPort: number,
 ): Promise<INestApplication> {
-  const app = await NestFactory.create(AppModule, options);
-  app.useWebSocketAdapter(new UWebSocketAdapter(app, { port: wsPort }));
+  let httpsOptions;
+  if (options.ssl) {
+    httpsOptions = {
+      key: readFileSync(options.ssl.key),
+      cert: readFileSync(options.ssl.cert),
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions, cors: true });
+  app.useWebSocketAdapter(
+    new UWebSocketAdapter(app, {
+      port: wsPort,
+      sslCert: options.ssl?.cert,
+      sslKey: options.ssl?.key,
+    }),
+  );
   await app.listen(port);
   return app;
 }
 
 async function bootstrapHttps(): Promise<void> {
-  const httpsOptions = {
-    key: readFileSync(`${__dirname}/../secrets/localhost.key`),
-    cert: readFileSync(`${__dirname}/../secrets/localhost.crt`),
+  const options = {
+    ssl: {
+      key: `${__dirname}/../secrets/localhost.key`,
+      cert: `${__dirname}/../secrets/localhost.crt`,
+    },
   };
-
-  await bootstrap({ cors: true, httpsOptions }, 3001, 8099);
+  await bootstrap(options, 3001, 8099);
 }
 
 async function bootstrapHttp(): Promise<void> {
